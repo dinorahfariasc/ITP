a853073c1d3a182da2325450bc4f9c2de4e248a8
dinorahfariasc <dinorahfarias@outlook.com>
2023-09-10 11:40:26 -0300

    commit

--- app.c
+++ app.c
@@ -22,9 +22,13 @@
 //=======================================================
 // Campeonato de skate
 #include <stdio.h>
+
 int melhorRodada(int skt[9]){
     int a, maior, menor;
     int notaskt[3];
+    int n1 = notaskt[0];
+    int n2 = notaskt[1];
+    int n3 = notaskt[2];
 
     for (int i = 3; i<=9; i = i + 3){
 
@@ -36,18 +40,19 @@ int melhorRodada(int skt[9]){
             if(maior > skt[x] && skt[x] > menor){
                 a = skt[x];
             }
-
-            if (skt[x] > maior){
+            else if (skt[x] > maior){
+                a = maior;
                 maior = skt[x];
             }
-
-            if (skt[x] < menor){
+            else if (skt[x] < menor){
+                a = menor;
                 menor = skt[x];
             }
-            
-            printf("***maior: %d menor: %d nota: %d\n",maior,menor,a);
+            else if(skt[x] == maior || skt[x] == menor){
+                a = skt[x];
+            } 
+            //printf("***maior: %d menor: %d nota: %d\n",maior,menor,a);
         }
-
         if (i == 3){
             notaskt[0] = a;
         }
@@ -56,47 +61,27 @@ int melhorRodada(int skt[9]){
         }
         if (i ==9){
             notaskt[2] = a;
-        }
-        
+        }     
     }
 
-    int final1 = notaskt[0];
-    maior = notaskt[0];
-    menor = notaskt[0];
+    n1 = notaskt[0];
+    n2 = notaskt[1];
+    n3 = notaskt[2];
 
-
-    for (int i = 0; i < 3; i++){
-        if (notaskt[i] > maior){
-            maior = notaskt[i];
-        }
-        if (notaskt[i] < menor){
-            menor = notaskt[i];
-            
-        }
-       
+    if ((n1 >= n2 && n1 <= n3) || (n1 <= n2 && n1 >= n3)) {
+        return n1;
+    } else if ((n2 >= n1 && n2 <= n3) || (n2 <= n1 && n2 >= n3)) {
+        return n2;
+    } else {
+        return n3;
     }
-
-    for (int i = 0; i < 3; i++) {
-        if (notaskt[i] != maior && notaskt[i] != menor) {
-            final1 = notaskt[i];
-            break;
-        }
-    }
-
-    //printf("cada rodada 1:%d 2:%d 3:%d \n",notaskt[0],notaskt[1],notaskt[2]);
-
-    return final1;
 }
-
-
-
 
 int main(){
 
     int notas[18];
     int skt1[9];
     int skt2[9];
-
 
     for (int i = 0; i < 18; i++){
         scanf("%d",&notas[i]);
@@ -108,8 +93,8 @@ int main(){
     }
 
     // chamar funcao melhorRodada
-    printf("melhor rodada skt1: %d\n",melhorRodada(skt1));
-    printf("melhor rodada skt2: %d\n",melhorRodada(skt2));
+    //printf("melhor rodada skt1: %d\n",melhorRodada(skt1));
+    //printf("melhor rodada skt2: %d\n",melhorRodada(skt2));
 
     if (melhorRodada(skt1) > melhorRodada(skt2)){
         printf("A");
